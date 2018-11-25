08b6982bb525e34a85fc0b0cef2819df78518951
Julian van Noordennen <34234772+Julianvannoordennen@users.noreply.github.com>
2018-11-25 17:38:39 +0100

    developer v0.3
    - Optimized app for mLab
    - Added database example data

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.Promise = global.Promise;
 //Check if we are on a test environment
 if (process.env.NODE_ENV !== 'test') {
     console.log(`Variable NODE_ENV is ${process.env.NODE_ENV}, application runs with the default database`);
-    mongoose.connect('mongodb://localhost/studdit', { useNewUrlParser: true });
+    mongoose.connect(process.env.STUDDIT_CONNECTION_URL, { useNewUrlParser: true })
 } else {
     console.log(`Variable NODE_ENV is ${process.env.NODE_ENV}, application runs with the test database`);
 }
--- models/example.js
+++ models/example.js
@@ -1,24 +1,24 @@
 /*
-
     ######################
     ### Example Schema ###
     ######################
-
-    const mongoose = require('mongoose');
-    const Schema = mongoose.Schema;
-
-    const DriverSchema = new Schema({
-        email: {
-            type: String,
-            required: true
-        },
-        driving: {
-            type: Boolean,
-            default: false
-        }
-    });
-
-    const Driver = mongoose.model('driver', DriverSchema);
-
-    module.exports = Driver;
 */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ExampleSchema = new Schema({
+    family: [{
+        name: String,
+        scienceName: String,
+        species: [{
+            name: String,
+            scienceName: String,
+            Extinct: Boolean
+        }]
+    }]
+});
+
+const Example = mongoose.model('penguins', ExampleSchema);
+
+module.exports = Example;
